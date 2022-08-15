8f9212c7b97486f4594896d26b2c8d926df612a4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-08-15 19:59:37 +0100

    Added sample mandelbrot route

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 from . import settings
+
+from my_library.my_module.my_functions import mandelbrot
 
 
 app = FastAPI()
@@ -16,3 +19,18 @@ async def homepage(request: Request):
     """
 
     return settings.templates.TemplateResponse("pages/index.html", {"request": request})
+
+
+class MandelbrotInput(BaseModel):
+    real: float
+    imag: float
+    max_iter: int = 80
+
+
+@app.get("/mandelbrot")
+async def sample_mandelbrot(input: MandelbrotInput):
+    """
+    Evaluate a single complex number.
+    """
+
+    return mandelbrot(complex(input.real, input.imag), input.max_iter)
