5e7122a37dc5561aafaf21bbea6adaff8c3fa16d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-08-15 21:15:44 +0100

    Added image display

--- app/static/client.js
+++ app/static/client.js
@@ -3,20 +3,4 @@ class Client {
     static go_to_splashpage() {
         window.location.replace("/");
     }
-
-    // == Sample ==
-    static mandelbrot(real, imag, success, error) {
-        $.ajax({
-            url: "/mandelbrot",
-            type: "GET",
-            dataType: "html",
-            contentType: "application/json",
-            data: JSON.stringify({
-                real: real,
-                imag: imag,
-            }),
-            success: success,
-            error: error,
-        });
-    }
 }
